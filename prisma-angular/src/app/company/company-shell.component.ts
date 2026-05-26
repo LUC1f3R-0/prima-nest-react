f94d6896e88d292c62118dfe752924ad08165e59
LUC1f3R-0 <thusharathilan123@gmail.com>
2026-05-26 12:02:44 +0530

    fixed

--- prisma-angular/src/app/company/company-shell.component.ts
+++ prisma-angular/src/app/company/company-shell.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-company-shell',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './company-shell.component.html',
   styleUrl: './company-shell.component.css',
 })
